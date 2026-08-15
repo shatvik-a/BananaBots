ff40943265fab937b1c38c7f60c3dc7b1bffa556
chakravaibhu07-eng <chakravaibhu07@gmail.com>
2026-08-15 12:44:18 -0500

    learning new terms
    I used a basketball game to understand the meaning of  the terms int, double, boolean, and String.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basketball.java
@@ -0,0 +1,16 @@
+public class basketball {
+    public static void main(String[] args) {
+        
+        int points = 4;// This means that I scored four points in the game
+        double shootingPercentage = 0.20;// This means that I scored 20% of my shots
+        boolean iwonGame = false;//This means that we lost the game
+        String Name = "Vaibhav";//This is my name
+
+        System.out.println("Player:" + Name);
+        System.out.println("Points:" + points);
+        System.out.println("Shooting Percentage:" + shootingPercentage);
+        System.out.println("Won Game:" + iwonGame);
+        
+
+    }
+}
